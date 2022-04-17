b6ef1af0f0e18dd8a47ad4303cc0f1278623978d
Ryan Wang <hwwangwang@gmail.com>
2022-04-18 00:24:54 +0800

    bump version

--- aiothrift/__init__.py
+++ aiothrift/__init__.py
@@ -1,12 +1,20 @@
 from thriftpy2 import load
-from .connection import create_connection, ThriftConnection
-from .pool import create_pool, ThriftPool
-from .errors import ConnectionClosedError, ThriftError, PoolClosedError, ThriftAppError
-from .processor import TProcessor
-from .server import Server, create_server
-from .protocol import TBinaryProtocol, TProtocol
 
-__version__ = "0.2.6"
+from .connection import create_connection
+from .connection import ThriftConnection
+from .errors import ConnectionClosedError
+from .errors import PoolClosedError
+from .errors import ThriftAppError
+from .errors import ThriftError
+from .pool import create_pool
+from .pool import ThriftPool
+from .processor import TProcessor
+from .protocol import TBinaryProtocol
+from .protocol import TProtocol
+from .server import create_server
+from .server import Server
+
+__version__ = "0.2.7"
 
 __all__ = [
     "create_connection",
